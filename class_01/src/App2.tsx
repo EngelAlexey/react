3c02848a5767dc938475d0bf7aeb4a62c69f506f
engelalexey <alexhmanzanares@gmail.com>
2025-06-06 20:29:05 -0600

    second commit

---
+++ class_01/src/App2.tsx
@@ -0,0 +1,30 @@
+function App2() {
+			  return (
+				<div className="container">
+					<h1>Las mejores 4 series que he visto</h1>
+					<p>
+						No suelo ser una persona muy aficionada a las series, entre ellas hay 4 que me gustan mucho <br/>
+						<ol>
+                            <li>Batman and Louis</li>
+                            <li>Invincible</li>
+                            
+
+                        </ol>
+                        
+
+						<br/>
+						<b>Usando JavaScript con el estándar ECMAScript 6 (ES6).</b>
+						<br/>
+						Entre los temas que se abordarán en esta clase se encuentran:
+					</p>
+					<ul>
+						<li>Declaración de variables y constantes.</li>
+						<li>Estructuras de control.</li>
+						<li>Declaración y uso de funciones.</li>
+						<li>Fundamentos de Programación Orientada a Objetos.</li>
+					</ul>
+				</div>
+			  );
+			}
+
+			export default App2;
