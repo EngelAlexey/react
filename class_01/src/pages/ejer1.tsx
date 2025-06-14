60aaa5c188cbab68dd28305834c36789cc73fc63
engelalexey <alexhmanzanares@gmail.com>
2025-06-13 18:44:25 -0600

    Commit

--- class_01/src/pages/ejer1.tsx
+++ class_01/src/pages/ejer1.tsx
@@ -4,25 +4,43 @@ const Ejer1 = () => {
                     <h1>Las mejores 4 series que he visto</h1>
 					<p>
 						No suelo ser una persona muy aficionada a las series, entre ellas hay 4 que me gustan mucho <br/>
-						<ol>
-                            <li>Batman and Louis</li>
-                            <li>Invincible</li>
-                            <li>The Boys</li>
-                            <li>Stranger Things</li>
-                        </ol>
-                        
-
-						<br/>
-						<b>Usando JavaScript con el estándar ECMAScript 6 (ES6).</b>
-						<br/>
-						Entre los temas que se abordarán en esta clase se encuentran:
 					</p>
-					<ul>
-						<li>Declaración de variables y constantes.</li>
-						<li>Estructuras de control.</li>
-						<li>Declaración y uso de funciones.</li>
-						<li>Fundamentos de Programación Orientada a Objetos.</li>
-					</ul>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <tr>
+                                        <td>
+                                            <img id = "superman_louis"
+                                                src="src/img/superman_and_louis.jpeg"
+                                                alt="Superman and Louis"
+                                                style={{ width: "200px", height: "200px" }}/>
+                                        </td>
+                                        <td>
+                                            <img id = "invincible"
+                                                src="src/img/invincible.jpeg"
+                                                alt="Invincible"
+                                                style={{ width: "200px", height: "200px" }}/>
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <img id = "the_boys"
+                                                src="src/img/the_boys.jpg"
+                                                alt="The Boys"
+                                                style={{ width: "200px", height: "200px" }}/>
+                                        </td>
+                                        <td>
+                                            <img id = "evangelion"
+                                                src="src/img/evangelion.jpg"
+                                                alt="Evangelion"
+                                                style={{ width: "200px", height: "200px" }}/>
+                                        </td>
+                                    </tr>
+                                </tr>
+                            </tbody>
+                        </table>
+                
 				</div>
 			);
 		}
